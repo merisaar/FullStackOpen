3fa2e12f130ed3c6a3b35d8884f203226ba250ef
Meri Saarinen <mtmsaa@utu.fi>
2018-09-13 13:08:08 +0300

    osio 1 tehtävät 1.1 ja 1.2

---
+++ osio1/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Otsikko} from './otsikko.js'
+import {Sisalto} from './sisalto.js'
+import {Yhteensa} from './yhteensa.js'
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi}/>
+      <Sisalto osa1={osa1}  osa2={osa2}  osa3={osa3} tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
+      <Yhteensa tehtavia1={tehtavia1} tehtavia2={tehtavia2} tehtavia3={tehtavia3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ osio1/src/yhteensa.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Yhteensa = (props) => {
+
+  return (
+    <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+  )
+}
